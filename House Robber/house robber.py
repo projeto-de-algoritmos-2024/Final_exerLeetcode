b24317937ad49f7c743a791324454685e07ea3fa
CarolCoCe <anaipva@gmail.com>
2024-09-09 14:23:59 -0300

    Deu tle mas os casos base passaram de primeira hehehe

--- House Robber/house robber.py
+++ House Robber/house robber.py
@@ -1,3 +1,8 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
-        pass
+        def recursiveRob (i):
+            if i >= len(nums):
+                return 0
+            return max(nums[i]+ recursiveRob(i+2), recursiveRob(i+1))
+        result = recursiveRob(0)
+        return result

b45a2b17c8a856b01bdcc4af259c9963814a8ff1
CarolCoCe <anaipva@gmail.com>
2024-09-09 14:29:37 -0300

    faltava memoization mas deu certo

--- House Robber/house robber.py
+++ House Robber/house robber.py
@@ -1,8 +1,12 @@
 class Solution:
     def rob(self, nums: List[int]) -> int:
+        memo = {}
         def recursiveRob (i):
             if i >= len(nums):
-                return 0
-            return max(nums[i]+ recursiveRob(i+2), recursiveRob(i+1))
+                return 0 
+            if i in memo:
+                return memo[i]
+            memo[i] = max(nums[i]+ recursiveRob(i+2), recursiveRob(i+1))
+            return memo[i]
         result = recursiveRob(0)
         return result
